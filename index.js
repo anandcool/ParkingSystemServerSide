db0156143d003904f1f78146491bd31780fc9ef5
anandcool <anand9412868527@gmail.com>
2020-08-09 05:40:19 -0700

    Add signup functionality

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post('/addLocation',(req,res) =>{
 
 })
 
-app.post('/signup',(req,res)=>{
+app.get('/signup',(req,res)=>{
     res.send(req.body)
     // const user = new User();
     // user.fname = req.body.fname;
