14aecce91adb4c2c7df623b605d2e0e9e2372c3c
anandcool <anand9412868527@gmail.com>
2020-08-09 21:27:28 -0700

    Add signup functionality1

--- index.js
+++ index.js
@@ -50,12 +50,12 @@ app.post('/login',(req,res) =>{
     User.find({email:email},(err,docs) =>{
         if(err) throw err;
         else{
-            // res.send(password)            
-            if(password == docs.pass){
-                res.status(200).json({msg:docs})
-            }else{
-                res.status(400).json({msg:'Password is not correct'})
-            }
+            res.send(password+"--"+docs.pass)            
+            // if(password == docs.pass){
+            //     res.status(200).json({msg:docs})
+            // }else{
+            //     res.status(400).json({msg:'Password is not correct'})
+            // }
         }
     })
 })
